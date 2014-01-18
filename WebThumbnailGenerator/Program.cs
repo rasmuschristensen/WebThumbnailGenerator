ccc0837e76fb7ac7762671a1efdd65f6fbdbc1d7
Rasmus Tolstrup Christensen <rtc@d60.dk>
2014-01-18 16:57:33 +0100

    added project

---
+++ WebThumbnailGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebThumbnailGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = @"phantomjs.exe";
+            proc.StartInfo.Arguments = string.Format("runner.js {0} {1}",
+                "http://www.onlinebooq.dk", "onlinebooq");
+            Console.WriteLine(proc.StartInfo.WorkingDirectory);
+            proc.StartInfo.WorkingDirectory = @"phantomjs";
+
+            proc.Start();
+        }
+    }
+}
