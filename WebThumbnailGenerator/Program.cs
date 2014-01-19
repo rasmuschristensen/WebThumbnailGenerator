38af99e51fbe77679af7d9d1f99cf6d2b70a5dfc
Rasmus Tolstrup Christensen <rtc@d60.dk>
2014-01-19 09:40:16 +0100

    woooot

--- WebThumbnailGenerator/Program.cs
+++ WebThumbnailGenerator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,43 @@ namespace WebThumbnailGenerator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ready to capture screenshot");
+
             var proc = new Process();
             proc.StartInfo.FileName = @"phantomjs.exe";
-            proc.StartInfo.Arguments = string.Format("runner.js {0} {1}",
-                "http://www.onlinebooq.dk", "onlinebooq");
+            proc.StartInfo.Arguments = "runner.js";
+            //proc.StartInfo.Arguments = string.Format("runner.js {0} {1}",
+            //    "http://www.onlinebooq.dk", "onlinebooq");
             Console.WriteLine(proc.StartInfo.WorkingDirectory);
             proc.StartInfo.WorkingDirectory = @"phantomjs";
+            
+            
 
-            proc.Start();
+
+            try
+            {
+                var processStarted = proc.Start();
+                var waitproc = proc.WaitForExit(15000);
+            
+            
+            Console.WriteLine("Process ran " + processStarted);
+            Console.WriteLine("Process wait " + waitproc);
+            Console.WriteLine("Done capturing screenshot");
+            Console.WriteLine("phantom exists? " + File.Exists("phantomjs/phantomjs.exe"));
+            Console.WriteLine("Image exists? " + File.Exists("phantomjs/webname.png"));
+            var files = Directory.GetFiles("phantomjs");
+            
+            string allfiles = string.Join(",", files);
+            Console.WriteLine("all files: " + allfiles);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine(e.StackTrace);
+
+            }
         }
     }
 }
--- WebThumbnailGenerator/phantomjs/runner.js
+++ WebThumbnailGenerator/phantomjs/runner.js
@@ -1,28 +1,16 @@
-var async = require('c:/dev/tools/phantomjs/async.js'),system = require('system');
-
-if (system.args.length === 1) {
-    console.log('Usage: runnerjs.js data');
-    phantom.exit();
-}
-
-var website = address = system.args[1];
-console.log(website);
-
-var webname = address = system.args[2];
-console.log(webname);
-
-
+console.log('woot');
+var system = require('system');
 var page = require('webpage').create();
-	//page.zoomFactor = 1;
-	page.viewportSize = {width: 1440, height: 900};
 
-	page.open(website, function () {    
-		var name = '../../haystack/Chef/Content/' + webname + '.png';
+console.log('baaaaam');
+
+page.viewportSize = { width: 1440, height: 900 };
+
+page.open("http://www.onlinebooq.dk", function () {
+    var name = 'webname' + '.png';
     page.render(name);
     console.log(name);
     page.close();
-   
-phantom.exit();
+
+    phantom.exit();
 });
-
-
